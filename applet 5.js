80ea9588523ebcaae9895b128d797d819b492884
DustinAguanta <dusitnagustin011@gmail.com>
2024-10-18 10:35:57 +0300

    added get location button

--- applet 5.js
+++ applet 5.js
@@ -2,5 +2,6 @@ class WeatherApp{
     constructor(){
         this.cityInput = document.getElementById('cityInput');
         this.getWeatherBtn = document.getElementById('getWeatherBtn');
+        this.getLocationBtn = document.getElementById('getLocationBtn');
     }
 }
