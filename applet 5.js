360c4bffe9eb3b05c203e60bc6022ddfe5295b86
DustinAguanta <dusitnagustin011@gmail.com>
2024-10-18 10:40:23 +0300

    added humidity

--- applet 5.js
+++ applet 5.js
@@ -7,5 +7,6 @@ class WeatherApp{
         this.cityName = document.getElementById('cityName');
         this.temperature = document.getElementById('temperature');
         this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
     }
 }
